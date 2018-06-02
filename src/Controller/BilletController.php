77c99fee6ac7f3f1502dd9d3c4840a6c422908d2
Anne Gasté <anne.gaste@hotmail.fr>
2018-06-02 21:09:43 +0200

    mise en place du formulaire billet

--- src/Controller/BilletController.php
+++ src/Controller/BilletController.php
@@ -4,16 +4,28 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Billet;
+use App\Form\BilletType;
 
 class BilletController extends Controller
 {
     /**
      * @Route("/billet", name="billet")
      */
-    public function index()
+    public function index(request $request)
     {
+        $em = $this->getDoctrine()->getManager();
+        $Billet = new Billet();
+        $form = $this->createForm(BilletType::class, $Billet);
+
+        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()){
+            $em->persist($Billet);
+            $em->flush();
+        }
+
         return $this->render('billet/index.html.twig', [
-            'controller_name' => 'BilletController',
+            'form' => $form->createView()
         ]);
     }
 }
---
+++ src/Form/BilletType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Billet;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BilletType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('type')
+            ->add('visit_date')
+            ->add('is_half')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Billet::class,
+        ]);
+    }
+}
