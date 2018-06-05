64abe68d8927ca334eacafff50012f9f5d5e4f34
Anne Gasté <anne.gaste@hotmail.fr>
2018-06-05 16:05:39 +0200

    modification des champs du formulaire visitor

--- src/Form/VisitorType.php
+++ src/Form/VisitorType.php
@@ -6,17 +6,20 @@ use App\Entity\Visitor;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+
 
 class VisitorType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('last_name')
-            ->add('first_name')
-            ->add('country')
-            ->add('bithdate')
-            ->add('email')
+            ->add('last_name', TextType::class, array('label' => 'Nom'))
+            ->add('first_name', TextType::class, array('label' => 'Prénom'))
+            ->add('country', TextType::class, array('label' => 'Pays'))
+            ->add('bithdate', DateTimeType::class, array('label' => 'Date de naissance'))
+            ->add('email', TextType::class, array('label' => 'Adresse mail'))
         ;
     }
 
