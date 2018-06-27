29d55d44115fc77aab6416d2df86b93609423379
Anne Gasté <anne.gaste@hotmail.fr>
2018-06-27 16:13:50 +0200

    restriction hotaire billet_type

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,24 +4,26 @@ import 'bootstrap-datepicker';
 
 const initDatePicker = () => {
 
-  $('#billet_visit_date').datepicker({
-    startDate: new Date(),
-    format: 'dd-mm-yyyy',
-    daysOfWeekDisabled: [2],
-    language: 'fr-FR',
-  });
+    $('#billet_visit_date').datepicker({
+        startDate: new Date(),
+        format: 'dd-mm-yyyy',
+        daysOfWeekDisabled: [2],
+        language: 'fr-FR',
+    });
 
-  $('#visitor_bithdate').datepicker({endDate: new Date(), format: 'dd-mm-yyyy', language: 'fr-FR'});
+    $('#visitor_bithdate').datepicker({endDate: new Date(), format: 'dd-mm-yyyy', language: 'fr-FR'});
 };
 
+
+
 const emptyFields = (formFields) => {
-  $(formFields).find('input').each(function () {
-    $(this).val('')
-  });
-  $(formFields).find('select').each(function () {
-    const firstOption = $($(this).find('option')[0]);
-    $(this).val(firstOption.attr('value'))
-  });
+    $(formFields).find('input').each(function () {
+        $(this).val('')
+    });
+    $(formFields).find('select').each(function () {
+        const firstOption = $($(this).find('option')[0]);
+        $(this).val(firstOption.attr('value'))
+    });
 };
 
 const getLastField = () => $('.fields')[$('.fields').length - 1];
@@ -29,70 +31,94 @@ const getLastField = () => $('.fields')[$('.fields').length - 1];
 const getNbFields = () => $('.fields').length;
 
 const setVisitorTitles = () => {
-  $('.title-visitor').each(function (idx) {
-    $(this).text(`Visiteur ${idx + 1}`);
-  });
+    $('.title-visitor').each(function (idx) {
+        $(this).text(`Visiteur ${idx + 1}`);
+    });
 };
 
 const duplicateFields = () => {
-  const newFields = $(getLastField()).clone();
-  const nbFields = getNbFields();
-  newFields.find('input').each(function(){
-    let currentId = $(this).attr('id');
-    currentId = currentId ? currentId.split('-')[0] : currentId
-    $(this).attr('id', `${currentId}-${nbFields + 1}`);
-  });
-  return newFields;
-};
 
-const setTarifPrice = (age) => {
-  if (age >= 0 && age < 4) {
-    $('#billet_type').val('gratuit');
-    $('#tarif_price').val(0);
-  }
-  if (age >= 5 && age < 12) {
-    $('#billet_type').val('enfant');
-    $('#tarif_price').val(8);
-  }
-  if (age >= 12 && age < 60) {
-    $('#billet_type').val('normal');
-    $('#tarif_price').val(16);
-  }
-  if (age >= 60) {
-    $('#billet_type').val('senior');
-    $('#tarif_price').val(12);
-  }
+    const newFields = $(getLastField()).clone();
+    const nbFields = getNbFields();
+    newFields.find('input').each(function () {
+        let currentId = $(this).attr('id');
+        currentId = currentId ? currentId.split('-')[0] : currentId
+        $(this).attr('id', `${currentId}-${nbFields + 1}`);
+
+    });
+    return newFields;
 };
 
 const getAge = () => {
-  const [day, month, year] = $('#visitor_bithdate').val().split('-');
-  const birthdate = new Date(year, month - 1, day);
-  return Math.round(Math.abs(new Date() - birthdate) / (1000 * 3600 * 24 * 365));
+    const [day, month, year] = $('#visitor_bithdate').val().split('-');
+    const birthdate = new Date(year, month - 1, day);
+    return Math.round(Math.abs(new Date() - birthdate) / (1000 * 3600 * 24 * 365));
 };
 
+const setTarifPrice = (age) => {
+    if (age >= 0 && age < 4) {
+        $('#tarif_name').val('gratuit');
+        $('#tarif_price').val(0);
+    }
+    if (age >= 5 && age < 12) {
+        $('#tarif_name').val('enfant');
+        $('#tarif_price').val(8);
+    }
+    if (age >= 12 && age < 60) {
+        $('#tarif_name').val('normal');
+        $('#tarif_price').val(16);
+    }
+    if (age >= 60) {
+        $('#tarif_name').val('senior');
+        $('#tarif_price').val(12);
+    }
+};
+
+const displayCurrentTime = () => {
+    const myDate = new Date();
+    const hours = myDate.getHours();
+
+    if (0 <= hours && hours <= 13)
+    {
+        $('#billet_type').val('journée');
+    }
+    else
+    {
+        $('#billet_type').val('demi-journée');
+    }
+
+};
+
+
 $(document).ready(function () {
+    displayCurrentTime();
+    initDatePicker();
 
-  initDatePicker();
-  $('#btnDel').attr('disabled', 'disabled');
-  setVisitorTitles();
-
-  $('#btnAdd').click(function () {
-    const newElem = duplicateFields();
-    emptyFields(newElem);
-    $(getLastField()).append(newElem);
-    if (getNbFields() > 1) {
-      $('#btnDel').attr('disabled', null);
-    }
+    $('#btnDel').attr('disabled', 'disabled');
     setVisitorTitles();
-  });
 
-  $('#btnDel').click(function () {
-    $(getLastField()).remove();
-    if (getNbFields() === 1) {
-      $('#btnDel').attr('disabled', 'disabled');
-    }
-    setVisitorTitles();
-  });
+    $('#btnAdd').click(function () {
+        const newElem = duplicateFields();
+        emptyFields(newElem);
+        $(getLastField()).append(newElem);
+        if (getNbFields() > 1) {
+            $('#btnDel').attr('disabled', null);
+        }
+        setVisitorTitles();
 
-  $('#visitor_bithdate').on('change', () => setTarifPrice(getAge()));
+    });
+
+    $('#btnDel').click(function () {
+        $(getLastField()).remove();
+        if (getNbFields() === 1) {
+            $('#btnDel').attr('disabled', 'disabled');
+        }
+        setVisitorTitles();
+    });
+
+    $('#visitor_bithdate').on('change', () => setTarifPrice(getAge()));
+
+
+
 });
+
